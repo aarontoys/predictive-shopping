9ec247c393952620d9e8637bb2a39171294af2d0
Aaron Toys <aarontoys@gmail.com>
2016-05-23 14:33:05 -0600

    fix typo in knex.js

--- src/server/db/knex.js
+++ src/server/db/knex.js
@@ -2,6 +2,6 @@
 var environment = process.env.NODE_ENV || 'development';
 
 console.log('environment', environment);
-var config = require('../../knexfile.js')[environment];
+var config = require('../../../knexfile.js')[environment];
 module.exports = require('knex')(config);
 
