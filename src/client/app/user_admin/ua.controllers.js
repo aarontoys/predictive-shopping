c077882b61bde50293682c5a23e836e70d7b623a
Aaron Toys <aarontoys@gmail.com>
2016-05-25 12:11:35 -0600

    create frontend view and controller for user admin

--- src/client/app/user_admin/ua.controllers.js
+++ src/client/app/user_admin/ua.controllers.js
@@ -6,11 +6,55 @@
 
   // userAdminCtrl.$inject = []
 
+
   function userAdminCtrl () {
     var vm = this;
 
-    console.log(vm);
+    console.log('line12',vm);
+    vm.mon=true;
+    vm.tue=true;
+    vm.wed=true;
+    vm.thu=true;
+    vm.fri=true;
+    vm.sat=true;
+    vm.sun=true;
+    // vm.days = [
+    //   {
+    //     text: 'Monday',
+    //     value: 1  
+    //   },
+    //   {
+    //     text: 'Tuesday',
+    //     value: 2 
+    //   },
+    //   {
+    //     text: 'Wednesday',
+    //     value: 3 
+    //   },
+    //   {
+    //     text: 'Thursday',
+    //     value: 4  
+    //   },
+    //   {
+    //     text: 'Friday',
+    //     value: 5  
+    //   },
+    //   {
+    //     text: 'Saturday',
+    //     value: 6  
+    //   },
+    //   {
+    //     text: 'Sunday',
+    //     value: 7  
+    //   },
+    // ]
+    function getUser () {
+      
+    }
 
+    vm.submit = function () {
+      console.log(vm);
+    }
   }
 
 })();
