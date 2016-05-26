0d3637537bfffe4a53c96d0ca4a743563e024a73
Aaron Toys <aarontoys@gmail.com>
2016-05-26 16:04:45 -0600

    rework schedule.js to accomodate the two types of schedules: by weekday & by day count

--- src/server/utils/schedule.js
+++ src/server/utils/schedule.js
@@ -3,22 +3,52 @@ var later = require('later');
 later.date.localTime();
 
 var sched = {};
+var listCount = 3;
+
+function createOccurances (int, arr) {
+  var occurances = []
+  switch(int) {
+    case 0:  
+      int = 'd'    
+      occurances = laterOccurances(createSchedule(int, arr));    
+      break;
+    case 1:      
+      date = arr[0];    
+      interval = arr[1];    
+      occurances = daySchedule(date, interval);    
+  }
+  return occurances
+}
+
+function daySchedule (startDate, interval) {
+  var arr = [];
+  startDate = new Date(startDate);
+  while(startDate  <= (new Date().getTime()+(interval*later.DAY*listCount))) {  
+    if (startDate >= later.day.start(new Date())) {
+      arr.push(startDate);
+    }  
+    startDate = new Date(startDate.getTime() + (interval*later.DAY));
+  }
+  if (arr.lenght === 4) {
+    arr.pop()
+  }
+  return arr;
+};
 
 function createSchedule (type, schedule) {
   sched = {
-    schedules: [{[type]: JSON.parse(schedule)}],
+    schedules: [{[type]: schedule}],
     exceptions: []
   }
   return sched;
 };
 
-function createOccurances (schedInst) {
+function laterOccurances (schedInst) {
   var d = new Date();
   var occurs = later.schedule(schedInst).next(3, d);
   return occurs;
 }
 
 module.exports = {
-  createSchedule: createSchedule,
   createOccurances: createOccurances
 }
