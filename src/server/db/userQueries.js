8ec3d8731275b06890492b3cb8c967fb5ce23ef0
Aaron Toys <aarontoys@gmail.com>
2016-05-25 19:04:08 -0600

    add new query to update user details in db

--- src/server/db/userQueries.js
+++ src/server/db/userQueries.js
@@ -9,7 +9,20 @@ function getSingleUser (id) {
   return Users().where('id',id);
 }
 
+function updateUser (id, fname, lname, email, schedule_type, schedule) {
+  console.log('line 13')
+  return Users().where('id',id)
+  .update({
+    fname: fname,
+    lname: lname,
+    email: email,
+    schedule_type: schedule_type,
+    schedule: JSON.stringify(schedule)
+  });
+}
+
 module.exports = {
   getAllUsers: getAllUsers,
-  getSingleUser: getSingleUser
+  getSingleUser: getSingleUser,
+  updateUser: updateUser
 }
