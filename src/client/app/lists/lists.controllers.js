cfca0545628e13e1be05394d77a1c090de684e3f
Aaron Toys <aarontoys@gmail.com>
2016-05-31 11:43:32 -0600

    update controller to default to user.id = 1

--- src/client/app/lists/lists.controllers.js
+++ src/client/app/lists/lists.controllers.js
@@ -9,19 +9,23 @@
   function listsCtrl (listsDataService) {
     var vm = this
     
-    vm.userId;
+    vm.userId =1;
     console.log(vm.userId);
 
-    // getLists(vm.userId);
+    getLists(vm.userId);
 
     vm.getLists = function (id) {
+      getLists(id);
+    }
+
+    // vm.getLists = function (id) {
+      function getLists (id) {
 
       listsDataService.getLists(id)
       .then(function (lists) {
         vm.lists = lists.data.lists;
         vm.items = lists.data.items;
         console.log(vm.lists);
-        console.log(vm.items);
       })
       .catch(function (err) {
         return next(err);
