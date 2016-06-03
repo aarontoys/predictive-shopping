0e959392a55c0f4546f985c616c58618b4767d04
Aaron Toys <aarontoys@gmail.com>
2016-06-02 22:13:28 -0600

    removed swig from app.js

--- src/server/app.js
+++ src/server/app.js
@@ -5,7 +5,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var swig = require('swig');
+// var swig = require('swig');
 
 
 // *** routes *** //
