17856237077d23c8f20abab8359f086a22f718d3
Aaron Toys <aarontoys@gmail.com>
2016-06-06 18:27:32 -0600

    troubleshooting heroku issues

--- src/server/routes/users.js
+++ src/server/routes/users.js
@@ -4,46 +4,47 @@ var router = express.Router();
 var userQueries = require('../db/userQueries');
 
 router.get('/', function (req, res, next) {
-  userQueries.getAllUsers()
-  .then(function (results) {
-    res.status(200).json({
-      status: 'success',
-      users: results
-    });
-  })
-  .catch(function (err) {
-    return next(err);
-  });
+  res.send('test');
+  // userQueries.getAllUsers()
+  // .then(function (results) {
+  //   res.status(200).json({
+  //     status: 'success',
+  //     users: results
+  //   });
+  // })
+  // .catch(function (err) {
+  //   return next(err);
+  // });
 });
 
-router.get('/:id', function (req, res, next) {
-  // console.log('id', req.pa)
-  userQueries.getSingleUser(req.params.id)
-  .then(function (result) {
-    res.status(200).json({
-      status: 'success',
-      user: result
-    });
-  })
-  .catch(function (err) {
-    return next(err);
-  });
-});
+// router.get('/:id', function (req, res, next) {
+//   // console.log('id', req.pa)
+//   userQueries.getSingleUser(req.params.id)
+//   .then(function (result) {
+//     res.status(200).json({
+//       status: 'success',
+//       user: result
+//     });
+//   })
+//   .catch(function (err) {
+//     return next(err);
+//   });
+// });
 
-router.post('/edit/:id', function (req, res, next) {
-  console.log('edit route');
-  var b = req.body
-  console.log(b);
-  userQueries.updateUser(req.params.id, b.fname, b.lname, b.email, b.schedule_type, b.schedule)
-  .then(function () {
-    // console.log('line39',result);
-    res.status(200).json({
-      status: 'success'
-    });
-  })
-  .catch(function (err) {
-    return next(err);
-  });
-});
+// router.post('/edit/:id', function (req, res, next) {
+//   console.log('edit route');
+//   var b = req.body
+//   console.log(b);
+//   userQueries.updateUser(req.params.id, b.fname, b.lname, b.email, b.schedule_type, b.schedule)
+//   .then(function () {
+//     // console.log('line39',result);
+//     res.status(200).json({
+//       status: 'success'
+//     });
+//   })
+//   .catch(function (err) {
+//     return next(err);
+//   });
+// });
 
 module.exports = router;
