2a473b2d75a88a232feedfaba3a4acb8bc59c313
Aaron Toys <aarontoys@gmail.com>
2016-06-08 00:08:57 -0600

    attempt to fix schedule bug - 15

--- src/server/utils/schedule.js
+++ src/server/utils/schedule.js
@@ -40,6 +40,9 @@ function daySchedule (startDate, interval) {
   var arr = [];
   startDate = new Date(startDate);
   endDate = (new Date(lastListDate).getTime() - startDate.getTime())/later.DAY
+  console.log('line43', lastListDate, !!lastListDate)
+  console.log('line43', startDate.getTime()+interval*5*later.DAY)
+  console.log('line45', new Date(lastListDate || startDate.getTime()+interval*5*later.DAY) )
   while(startDate  <= new Date(lastListDate || startDate.getTime()+interval*5*later.DAY)) {  
     if (startDate >= later.day.start(new Date())) {
       arr.push(later.day.end(startDate));
